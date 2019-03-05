a9f40d941522f5d3541347e413196d9e77194674
tadataku-lab <tadataku.lab@gmail.com>
2019-03-05 16:19:31 +0900

    fet: add ace editor

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,3 +3,9 @@ import { hello } from './sub';
 const message: string = 'Hello World';
 
 hello(message);
+
+(function(){
+  var editor = ace.edit("editor");
+  //editor.setTheme("ace/theme/solarized_light");
+  //editor.getSession().setUseWrapMode(true);
+})()
